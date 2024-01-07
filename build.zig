0ac3f14488921016d2713906dfd0442fdc82c2a2
KNnut <9387720+KNnut@users.noreply.github.com>
2024-01-07 12:51:59 +0800

    Update to zig 0.12.0-dev.2059+42389cb9c

--- build.zig
+++ build.zig
@@ -1,14 +1,12 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) !void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const module = b.createModule(.{
-        .source_file = .{ .path = "bincode.zig" },
+    _ = b.addModule("bincode", .{
+        .root_source_file = .{ .path = "bincode.zig" },
     });
-
-    try b.modules.put(b.dupe("bincode"), module);
 
     const lib = b.addStaticLibrary(.{
         .name = "bincode",
